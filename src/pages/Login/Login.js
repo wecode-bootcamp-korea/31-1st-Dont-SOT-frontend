fb3d80008b84436771f7bb4458ecb604529c5f0e
Geuni <dlrmsgnl620@naver.com>
2022-03-29 01:28:26 +0900

    Add: Login 기본 레이아웃 구성

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Login.scss';
 
 const Login = () => {
-  return <div>login</div>;
+  return (
+    <section>
+      <div className="login">
+        <h2 className="medium">로그인</h2>
+        <form className="loginForm">
+          <input type="text" placeholder="아이디" />
+          <input type="password" placeholder="비밀번호" />
+          <div className="loginCheck">
+            <div className="loginCheckBox">
+              <input type="checkbox" />
+              아이디 저장
+            </div>
+            <span>
+              <Link to="#">아이디/비밀번호 찾기</Link>
+            </span>
+          </div>
+          <button>로그인</button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
