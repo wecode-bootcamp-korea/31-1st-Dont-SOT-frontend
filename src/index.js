1fcb3cef462792cfb5310822516a3fb5dd3c5a5b
Kyungsuh Park <pyo05360@gmail.com>
2022-03-28 19:26:56 +0900

    Modify: 초기 세팅 수정

--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ import Router from './Router';
 
 import './styles/reset.scss';
 import './styles/common.scss';
-import './styles/variables.scss';
 
 ReactDOM.render(<Router />, document.getElementById('root'));
