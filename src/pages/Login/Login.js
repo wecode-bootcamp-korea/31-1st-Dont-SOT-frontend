cb10a1bf0f2b0e7e10d1eb510e6b72d0c530f92d
Geuni <dlrmsgnl620@naver.com>
2022-03-29 19:38:32 +0900

    Add: 레이아웃 inner 추가

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -22,46 +22,48 @@ const Login = () => {
 
   return (
     <section className="login">
-      <div className="loginPage">
-        <form className="loginForm">
-          <h2 className="medium">로그인</h2>
-          <input
-            type="text"
-            name="id"
-            placeholder="아이디"
-            onChange={handleInputs}
-            value={inputs.id}
-          />
-          <input
-            type="password"
-            name="pw"
-            placeholder="비밀번호"
-            onChange={handleInputs}
-            value={inputs.pw}
-          />
-          <div className="loginCheck">
-            <div className="loginCheckBox">
-              <input type="checkbox" />
-              아이디 저장
+      <div className="inner">
+        <div className="loginPage">
+          <form className="loginForm">
+            <h2 className="medium">로그인</h2>
+            <input
+              type="text"
+              name="id"
+              placeholder="아이디"
+              onChange={handleInputs}
+              value={inputs.id}
+            />
+            <input
+              type="password"
+              name="pw"
+              placeholder="비밀번호"
+              onChange={handleInputs}
+              value={inputs.pw}
+            />
+            <div className="loginCheck">
+              <div className="loginCheckBox">
+                <input type="checkbox" />
+                아이디 저장
+              </div>
+              <span>
+                <Link to="#">아이디/비밀번호 찾기</Link>
+              </span>
             </div>
-            <span>
-              <Link to="#">아이디/비밀번호 찾기</Link>
-            </span>
+            <button>로그인</button>
+          </form>
+        </div>
+        <div className="joinPage">
+          <span>
+            <img src="https://avatars.githubusercontent.com/u/56650238?v=4" />
+          </span>
+          <div className="joinSelect">
+            <h3 className="medium">솥지마요 회원이 아니신가요?</h3>
+            <p>
+              회원이 되시면 솥지마요의 다양한 정보와 이벤트를 만나보실 수
+              있습니다.
+            </p>
+            <button>회원가입</button>
           </div>
-          <button>로그인</button>
-        </form>
-      </div>
-      <div className="joinPage">
-        <span>
-          <img src="https://avatars.githubusercontent.com/u/56650238?v=4" />
-        </span>
-        <div className="joinSelect">
-          <h3 className="medium">솥지마요 회원이 아니신가요?</h3>
-          <p>
-            회원이 되시면 솥지마요의 다양한 정보와 이벤트를 만나보실 수
-            있습니다.
-          </p>
-          <button>회원가입</button>
         </div>
       </div>
     </section>
