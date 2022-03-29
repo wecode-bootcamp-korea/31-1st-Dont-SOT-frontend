99c4cd3e341cf13a71dd5824bdb0f06b85a3b14f
Geuni <dlrmsgnl620@naver.com>
2022-03-29 13:11:04 +0900

    Add: 로그인 아이콘 가상요소 추가 및 유효성 input값 받기 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,15 +1,42 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 const Login = () => {
+  // 로그인 input 값 받기
+  let [inputs, setInputs] = useState({
+    id: '',
+    pw: '',
+  });
+
+  const handleInputs = e => {
+    const { name, value } = e.target;
+
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
   return (
     <section>
       <div className="login">
-        <h2 className="medium">로그인</h2>
         <form className="loginForm">
-          <input type="text" placeholder="아이디" />
-          <input type="password" placeholder="비밀번호" />
+          <h2 className="medium">로그인</h2>
+          <input
+            type="text"
+            name="id"
+            placeholder="아이디"
+            onChange={handleInputs}
+            value={inputs.id}
+          />
+          <input
+            type="password"
+            name="pw"
+            placeholder="비밀번호"
+            onChange={handleInputs}
+            value={inputs.pw}
+          />
           <div className="loginCheck">
             <div className="loginCheckBox">
               <input type="checkbox" />
@@ -21,6 +48,18 @@ const Login = () => {
           </div>
           <button>로그인</button>
         </form>
+      </div>
+      <div className="join">
+        {/* 이미지 넣을 것 */}
+        <img alt="backgroundImg" />
+        <div className="joinSelect">
+          <h3>솥지마요 회원이 아니신가요?</h3>
+          <p>
+            회원이 되시면 솥지마요의 다양한 정보와 이벤트를 만나보실 수
+            있습니다.
+          </p>
+          <button>회원가입</button>
+        </div>
       </div>
     </section>
   );
