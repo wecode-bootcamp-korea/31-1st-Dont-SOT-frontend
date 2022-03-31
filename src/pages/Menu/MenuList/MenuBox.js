1bb73c6394b1593d4edd33c910d9c65da0a03992
Kyungsuh Park <pyo05360@gmail.com>
2022-03-31 16:27:09 +0900

    Modify: mock data 수정, MenuBox 리팩토링

--- src/pages/Menu/MenuList/MenuBox.js
+++ src/pages/Menu/MenuList/MenuBox.js
@@ -5,19 +5,19 @@ const MenuBox = props => {
   const { category, categoryList } = props;
   return (
     <article className="listWrap">
-      <div className="header">
-        {categoryList.map(data => {
-          return (
-            <div key={data.id}>
+      {categoryList.map(data => {
+        return (
+          <>
+            <div className="header" key={data.id}>
               <p className="cateTitle">{category}</p>
               <h3 className="cateTitleItem">{data.miniCategory}</h3>
-              <ul className="menulistBox">
-                <Card contents={data.contents} />
-              </ul>
             </div>
-          );
-        })}
-      </div>
+            <ul className="menulistBox">
+              <Card contents={data.contents} />
+            </ul>
+          </>
+        );
+      })}
     </article>
   );
 };
