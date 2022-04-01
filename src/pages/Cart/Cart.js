bd1ad257d81c6a9c9853f7951b01192a7dce5c7d
MAGHC <89845540+MAGHC@users.noreply.github.cog>
2022-04-01 17:54:23 +0900

    wip

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -8,18 +8,33 @@ const Cart = () => {
       <ul>
         <CartList />
       </ul>
-      <div className="totalPrice">
-        <h3>결제금액</h3>
-        <p className="allprice">총 결제 금액</p>
-        <span>총금액</span>
-        <span>원</span>
-        <p className="allprice">배송비</p>
-        <span>3000원</span>
-        <h2>전체 주문 금액</h2>
-        <span>배송비합총액</span>
-        <span>원</span>
+      <div className="total">
+        <div className="totalPrice">
+          <div className="allPrice" />
+          <h3>결제금액</h3>
+          <span>총 결제 금액</span>
+          <div className="right">
+            <span className="allpriceRight">총금액</span>
+            <span>원</span>
+          </div>
+          <div>
+            <div className="beverage">
+              <p className="allprice">배송비</p>
+              <div className="right">
+                <span>3000원</span>
+              </div>
+            </div>
+          </div>
+          <div className="totalprice3">
+            <h2>전체 주문 금액</h2>
+            <div className="right">
+              <span>배송비합총액</span>
+              <span>원</span>
+            </div>
+          </div>
+        </div>
+        <button>주문</button>
       </div>
-      <button>주문</button>
     </div>
   );
 };
