42f2a773fbe7ef75e9d58a64a0fc989eaf45dc8e
hdh <ghdengus12@naver.com>
2022-04-01 13:36:17 +0900

    add: nav 페이지 스크롤 기능 구현 완료

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,25 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Nav.scss';
 import './navData';
 import NAV_LIST from './navData';
 import NavList from './NavList';
 
 const Nav = () => {
+  const [navActive, setNavActive] = useState(false);
+  const [scrollY, setScrollY] = useState(0);
+  const [scrollActive, setScrollActive] = useState(false);
+
+  const scrollFixed = () => {
+    if (scrollY > 100) {
+      setScrollY(window.pageYOffset);
+      setScrollActive(true);
+    } else {
+      setScrollY(window.pageYOffset);
+      setScrollActive(false);
+    }
+  };
+
+  useEffect(() => {
+    const scrollListener = () => {
+      window.addEventListener('scroll', scrollFixed);
+    };
+    scrollListener();
+    return () => {
+      window.removeEventListener('scroll', scrollFixed);
+    };
+  });
   return (
-    <div className="nav">
+    <div className={'nav' + (scrollActive ? ' fixed' : '')}>
       <ul className="navUtil inner">
         <li>로그인</li>
         <li>회원가입</li>
         <li>
-          <span>인스타</span>
-          <span>페이스북</span>
+          <span className="instagram">인스타</span>
+          <span className="facebook">페이스북</span>
         </li>
       </ul>
-      <div className="navWrap inner">
-        <h1 className="logo">
-          <img src="/images/logo2.png" alt="" />
-        </h1>
-        <ul className="navListWrap">
+      <div
+        className="navWrap inner"
+        onMouseLeave={() => {
+          setNavActive(false);
+        }}
+      >
+        <h1 className="logo">로고이미지</h1>
+        <ul
+          className={'navListWrap' + (navActive ? ' active' : '')}
+          onMouseEnter={() => {
+            setNavActive(true);
+          }}
+        >
           {NAV_LIST.map(list => (
             <NavList key={list.id} {...list} />
           ))}
