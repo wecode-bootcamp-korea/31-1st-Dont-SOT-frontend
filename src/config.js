f6a1c991484492a12c99488a0b26a9904b0c661e
Geuni <dlrmsgnl620@naver.com>
2022-04-02 16:52:27 +0900

    Modify: config 파일 형식 변경

--- src/config.js
+++ src/config.js
@@ -1,9 +1,6 @@
-import React from 'react';
-
-const config = () => {
-  const BASE_URL = 'http://10.58.7.71:8000';
-  const API = { Login: `${BASE_URL}/users/signin` };
-  return <div />;
+const BASE_URL = 'http://10.58.7.71:8000';
+const API = {
+  Login: `${BASE_URL}/users/signin`,
 };
 
-export default config;
+export default API;
