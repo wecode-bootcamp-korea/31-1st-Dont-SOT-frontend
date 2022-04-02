b70da4345925a2818ab17515777826c7548d241d
Geuni <dlrmsgnl620@naver.com>
2022-04-02 16:32:52 +0900

    ReFactor: 리뷰 반영(주석/useState/naming/config/alt/className)

---
+++ src/config.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const config = () => {
+  const BASE_URL = 'http://10.58.7.71:8000';
+  const API = { Login: `${BASE_URL}/users/signin` };
+  return <div />;
+};
+
+export default config;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,33 +1,33 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Login.scss';
+import API from '../../config';
 
 const Login = () => {
   const navigate = useNavigate();
-  // 로그인 input 값 받기
-  let [inputs, setInputs] = useState({
-    id: '',
-    pw: '',
+
+  const [loginInputs, setLoginInputs] = useState({
+    idValue: '',
+    pwValue: '',
   });
 
   const handleInputs = e => {
     const { name, value } = e.target;
 
-    setInputs({
-      ...inputs,
+    setLoginInputs({
+      ...loginInputs,
       [name]: value,
     });
   };
 
-  const isVaild = inputs.id && inputs.pw !== '';
+  const isLoginVaild = loginInputs.id && loginInputs.pw !== '';
 
-  // 로그인 인증 & 인가
   const gotoMain = () => {
-    fetch('http://10.58.7.71:8000/users/signin', {
+    fetch(`${API.Login}`, {
       method: 'POST',
       body: JSON.stringify({
-        username: inputs.id,
-        password: inputs.pw,
+        username: loginInputs.id,
+        password: loginInputs.pw,
       }),
     })
       .then(res => res.json())
@@ -47,34 +47,36 @@ const Login = () => {
       <div className="inner">
         <div className="loginPage">
           <form className="loginForm">
-            <h2 className="medium">로그인</h2>
+            <h2 className="medium loginTitle">로그인</h2>
             <input
+              className="loginInput"
               type="text"
               name="id"
               placeholder="아이디"
               onChange={handleInputs}
-              value={inputs.id}
+              value={loginInputs.id}
             />
             <input
+              className="pwInput"
               type="password"
               name="pw"
               placeholder="비밀번호"
               onChange={handleInputs}
-              value={inputs.pw}
+              value={loginInputs.pw}
             />
             <div className="loginCheck">
               <div className="loginCheckBox">
                 <input type="checkbox" />
                 아이디 저장
               </div>
-              <span>
+              <span className="loginFind">
                 <Link to="/join">아이디/비밀번호 찾기</Link>
               </span>
             </div>
             <button
               type="button"
-              className={isVaild ? 'active' : null}
-              disabled={!isVaild}
+              className={isLoginVaild ? 'active' : null}
+              disabled={!isLoginVaild}
               onClick={gotoMain}
             >
               로그인
@@ -83,15 +85,18 @@ const Login = () => {
         </div>
         <div className="joinPage">
           <span>
-            <img src="https://avatars.githubusercontent.com/u/56650238?v=4" />
+            <img
+              alt="backgroundImg"
+              src="https://avatars.githubusercontent.com/u/56650238?v=4"
+            />
           </span>
           <div className="joinSelect">
-            <h3 className="medium">솥지마요 회원이 아니신가요?</h3>
-            <p>
+            <h3 className="medium joinTitle">솥지마요 회원이 아니신가요?</h3>
+            <p className="joinSubTitle">
               회원이 되시면 솥지마요의 다양한 정보와 이벤트를 만나보실 수
               있습니다.
             </p>
-            <button>회원가입</button>
+            <button className="joinBtn">회원가입</button>
           </div>
         </div>
       </div>
