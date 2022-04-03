74cb1279f0949bf5a559bfebf50802ad4342a4e2
Geuni <dlrmsgnl620@naver.com>
2022-04-03 16:48:11 +0900

    Add: config 파일 MenuList URL 추가

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ const API = {
   Login: `${BASE_URL}/users/signin`,
   Join: `${BASE_URL}/users/signin/signup`,
   LoginCheck: `${BASE_URL}/users/signin/idcheck`,
+  MenuList: `${BASE_URL}/products/`,
 };
 
 export default API;
