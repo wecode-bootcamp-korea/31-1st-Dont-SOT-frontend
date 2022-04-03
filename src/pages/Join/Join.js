a23045af8336fb451df4e037dc46b1951f050c10
Geuni <dlrmsgnl620@naver.com>
2022-04-03 20:48:56 +0900

    Add: import config경로 추가

--- src/pages/Join/Join.js
+++ src/pages/Join/Join.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Form from './Form/Form';
 import '../Join/Join.scss';
+import API from '../../config';
 
 const Join = () => {
   const navigate = useNavigate();
