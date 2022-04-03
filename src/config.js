753b23a05579f24de23b583267950bb083337908
Geuni <dlrmsgnl620@naver.com>
2022-04-03 17:28:33 +0900

    Modify: 아이디중복 key 값 수정

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const BASE_URL = 'http://10.58.7.71:8000';
 const API = {
   Login: `${BASE_URL}/users/signin`,
   Join: `${BASE_URL}/users/signin/signup`,
-  LoginCheck: `${BASE_URL}/users/signin/idcheck`,
+  JoinId: `${BASE_URL}/users/signin/idcheck`,
   MenuList: `${BASE_URL}/products/`,
 };
 
