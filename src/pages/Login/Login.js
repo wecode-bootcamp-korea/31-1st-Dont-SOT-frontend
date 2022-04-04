370192690dbf281c6c9d1bae9efc427fadb8db94
Geuni <dlrmsgnl620@naver.com>
2022-04-04 13:48:00 +0900

    Refactor: 수정사항 반영

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import './Login.scss';
 import API from '../../config';
+import './Login.scss';
 
 const Login = () => {
   const navigate = useNavigate();
