fdf659729e5a45ffe7ca073935362b408cfa70b8
hdh <ghdengus12@naver.com>
2022-04-04 18:29:10 +0900

    modify : main에서만 보이는 가상 요소 작업

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import NAV_LIST from './navData';
 import './Nav.scss';
 
 const Nav = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   const [navActive, setNavActive] = useState(false);
   const [scrollY, setScrollY] = useState(0);
   const [scrollActive, setScrollActive] = useState(false);
@@ -27,51 +28,56 @@ const Nav = () => {
     return () => {
       window.removeEventListener('scroll', scrollFixed);
     };
-  });
+  }, []);
+
+  const isMainPage = location.pathname === '/';
 
   return (
-    <div className={`nav${scrollActive ? ' fixed' : ''}`}>
-      <ul className="navUtil inner">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>
-          <span className="instagram">인스타</span>
-          <span className="facebook">페이스북</span>
-        </li>
-      </ul>
-      <div
-        className="navWrap inner"
-        onMouseLeave={() => {
-          setNavActive(false);
-        }}
-      >
-        <h1 className="logo">로고이미지</h1>
-        <ul
-          className={`navListWrap${navActive ? ' active' : ''}`}
-          onMouseEnter={() => {
-            setNavActive(true);
+    <>
+      <div className={`nav${scrollActive ? ' fixed' : ''}`}>
+        <ul className="navUtil inner">
+          <li>로그인</li>
+          <li>회원가입</li>
+          <li>
+            <span className="instagram">인스타</span>
+            <span className="facebook">페이스북</span>
+          </li>
+        </ul>
+        <div
+          className="navWrap inner"
+          onMouseLeave={() => {
+            setNavActive(false);
           }}
         >
-          {NAV_LIST.map(item => (
-            <li className="navList" key={item.id}>
-              <p className="listTitle bold">{item.category}</p>
-              <ul className="listWrap">
-                {item.list.map(list => (
-                  <li
-                    className="list"
-                    key={list.id}
-                    onClick={() => navigate(list.path)}
-                  >
-                    {list.data}
-                  </li>
-                ))}
-              </ul>
-            </li>
-          ))}
-        </ul>
-        <div className="dim" />
+          <h1 className="logo">로고이미지</h1>
+          <ul
+            className={`navListWrap${navActive ? ' active' : ''}`}
+            onMouseEnter={() => {
+              setNavActive(true);
+            }}
+          >
+            {NAV_LIST.map(item => (
+              <li className="navList" key={item.id}>
+                <p className="listTitle bold">{item.category}</p>
+                <ul className="listWrap">
+                  {item.list.map(list => (
+                    <li
+                      className="list"
+                      key={list.id}
+                      onClick={() => navigate(list.path)}
+                    >
+                      {list.data}
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+          <div className="dim" />
+        </div>
       </div>
-    </div>
+      {!isMainPage && <div className="virtualNav" />}
+    </>
   );
 };
 
