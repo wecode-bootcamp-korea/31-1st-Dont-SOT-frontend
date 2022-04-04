98595c25f8ca81ab01ea3ed65844173b5fa90598
MAGHC <89845540+MAGHC@users.noreply.github.cog>
2022-04-04 15:54:41 +0900

    add:장바구니 레이아웃 및 기능추가구현중

---
+++ src/pages/Cart/Cart.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from 'react';
+import CartList from './CartItem/CartItem';
+import '../Cart/Cart.scss';
+
+const Cart = () => {
+  const [cartList, setCartList] = useState([]);
+  const [preTotal, setPreTotal] = useState([]);
+  const [cartTotal, setCartTotal] = useState(3000);
+
+  // const presum = cartList.reduce((a, b) => {
+  //   const { price, quantity } = b;
+  //   let sum = a + price * quantity;
+  //   return setPreTotal(sum);
+  // });
+
+  console.log(preTotal);
+  // const [item , setItem]=useState()
+  // const [validation, setValidation] = useState(false);
+  // const token = [localStorage.getItem('token')];
+  // console.log(preTotal);
+  useEffect(() => {
+    // if (!token) {
+    //   setValidation(true);
+    // }
+
+    fetch('http://localhost:3000/data/cartData.json', {
+      // headers: {
+      //   method: 'GET',
+      //   Authorization: token,
+      // },
+      method: 'GET',
+    })
+      .then(res => res.json())
+      .then(data => setCartList(data));
+  }, []);
+
+  return (
+    <div className="cart">
+      <div className="cartHead bold">
+        <h2>장바구니</h2>
+      </div>
+      <ul>
+        {cartList.map(list => {
+          return (
+            <CartList
+              id={list.id}
+              key={list.id}
+              price={list.price}
+              option={list.option}
+              quantity={list.quantity}
+              // updatePrice={updatePrice}
+            />
+          );
+        })}
+      </ul>
+      <h3 className="priceHead">결제금액</h3>
+      <div className="priceArea">
+        <p className="widtharea">총 결제 금액</p>
+        <div className="right">
+          <span className="allpriceRight bold">총금액</span>
+          <span>원</span>
+        </div>
+      </div>
+      <div>
+        <div className="priceArea">
+          <p className="widtharea">배송비</p>
+          <div className="right">
+            <span>3000</span>
+            <span>원</span>
+          </div>
+        </div>
+      </div>
+      <div className="priceArea">
+        <h2 className="widtharea last">전체 주문 금액</h2>
+        <div className="right">
+          <span className="bold">배송비합총액</span>
+          <span>원</span>
+        </div>
+      </div>
+      {/* <button>주문</button> */}
+    </div>
+  );
+};
+
+export default Cart;
---
+++ src/pages/Cart/CartItem/CartItem.js
@@ -0,0 +1,67 @@
+import { useState } from 'react';
+import '../CartItem/CartItem.scss';
+
+const CartItem = ({ id, price, option, updatePrice, quantity }) => {
+  const [count, setCount] = useState(1);
+  const [prePrice, setPrePrice] = useState(price * quantity);
+  // const [preTotal, setPreTotal] = useState([]);
+  const handleBtn = e => {
+    const { value, id } = e.target;
+    setCount(count => ({ ...count, [id]: value }));
+  };
+
+  function addQuan() {
+    if (count < 9) {
+      setCount(count + 1);
+      setPrePrice(price + price * count);
+      // setPreTotal(preTotal => ({ ...preTotal, [id]: prePrice + price }));
+      updatePrice(prePrice + price);
+    } else {
+      alert('9개까지 주문가능합니다');
+    }
+  }
+
+  function minusQuan() {
+    if (count > 1) {
+      setCount(count - 1);
+      setPrePrice((price - price * count) * -1);
+      // setPreTotal(preTotal => ({ ...preTotal, [id]: prePrice - price }));
+    }
+  }
+  return (
+    <li key={id}>
+      <div className="cartItem">
+        <div className="mainBox">
+          <div className="imgbox">
+            <img src="images\slide-img01.jpg" alt="상품이미지" />
+          </div>
+        </div>
+        <div className="orederInfo">
+          <dt className="bold">상품명</dt>
+          <dd>상품내용</dd>
+          <dd>{option && '곱빼기'}</dd>
+        </div>
+        <div className="orederPrice">
+          <span id={id}>{prePrice}</span>
+          <span>원</span>
+          <div className="CounterButton">
+            <button type="button" className="minusButton" onClick={minusQuan}>
+              -
+            </button>
+            <input
+              type="number"
+              className="count bold"
+              value={count}
+              onChange={handleBtn}
+            />
+            <button type="button" className="plusButton" onClick={addQuan}>
+              +
+            </button>
+          </div>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
