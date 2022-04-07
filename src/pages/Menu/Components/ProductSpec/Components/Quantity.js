3a7264e36b09f89598aae1ba89f31645a1fe0959
hdh <ghdengus12@naver.com>
2022-04-07 16:21:52 +0900

    modify : 원산지, 알러지, 열량 API 통신 완료

--- src/pages/Menu/Components/ProductSpec/Components/Quantity.js
+++ src/pages/Menu/Components/ProductSpec/Components/Quantity.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Quantity.scss';
 
-const Quantity = () => {
+const Quantity = ({ calory }) => {
   return (
     <div className="menuInfo quantity">
       <h4 className="h4Tit">열량</h4>
       <p className="kcalBox">
-        <span className="kcalValue">872,73</span>Kcal
+        <span className="kcalValue">{calory}</span>Kcal
       </p>
     </div>
   );
